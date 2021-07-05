f3906a187575840d2e5acee47aa41baa182b9300
Simakov Nikita <simakovnikita4@gmail.com>
2021-07-05 16:07:44 +0600

    [+] Added math util class Pair

---
+++ src/main/java/heroes/mathutils/Pair.java
@@ -0,0 +1,36 @@
+package heroes.mathutils;
+
+import java.util.Objects;
+
+public final class Pair<T, U> {
+
+    private final T x;
+    private final U y;
+
+    public Pair(final T x, final U y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public T getX() {
+        return x;
+    }
+
+    public U getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(x, pair.x) &&
+                Objects.equals(y, pair.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
