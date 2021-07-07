8b37f3f887c5a4a57a6ebb5698879992dca98707
Simakov Nikita <simakovnikita4@gmail.com>
2021-07-07 11:29:01 +0600

    [+] Added logger (not configured yet)

--- src/main/java/heroes/boardfactory/DamageCommand.java
+++ src/main/java/heroes/boardfactory/DamageCommand.java
@@ -1,14 +1,19 @@
 package heroes.boardfactory;
 
 import heroes.units.Unit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class DamageCommand extends Command {
+    Logger logger = LoggerFactory.getLogger(DamageCommand.class);
     public DamageCommand(Unit att, Unit def) {
         super(att, def);
 
     }
     @Override
     public void execute() {
+        logger.info("Dealing damage");
         System.out.println("Damaging...");
     }
 }
