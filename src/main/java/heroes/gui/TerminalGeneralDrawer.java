1b7d598ca310f038e9d2b573b1e6d52f6f4e3fe0
Simakov Nikita <simakovnikita4@gmail.com>
2021-07-20 17:16:21 +0600

    [+] Added general names on screen.

--- src/main/java/heroes/gui/TerminalGeneralDrawer.java
+++ src/main/java/heroes/gui/TerminalGeneralDrawer.java
@@ -1,10 +1,15 @@
 package heroes.gui;
 
-import heroes.auxiliaryclasses.ActionTypes;
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import heroes.gui.utils.GeneralDrawersMap;
+import heroes.gui.utils.GeneralNamesMap;
 import heroes.gui.utils.Side;
+import heroes.gui.utils.TextColorMap;
+import heroes.units.General;
 
 import java.io.IOException;
+import java.util.EnumSet;
 
 /**
  * Статический класс для рисования портретов генералов.
@@ -19,7 +24,18 @@ public class TerminalGeneralDrawer {
      * @throws IOException исключение из методов Лантерны пробрасывается выше.
      */
     public static void drawGeneral(final TerminalWrapper tw,
-                                   final ActionTypes general, final Side side) throws IOException {
-        GeneralDrawersMap.getDrawer(general).draw(tw, side);
+                                   final General general, final Side side) throws IOException {
+        GeneralDrawersMap.getDrawer(general.getActionType()).draw(tw, side);
+        drawStatus(tw, general, side);
+    }
+
+    private static void drawStatus(final TerminalWrapper tw, final General general, final Side side) throws IOException {
+        int x_start = (side == Side.RHS) ? tw.getTerminal().getTerminalSize().getColumns() - 34 : 1;
+        int y_start = 25;
+        TextGraphics tg = tw.getScreen().newTextGraphics();
+        tg.setForegroundColor(TextColorMap.getColor("gold"));
+        tg.setModifiers(EnumSet.of(SGR.ITALIC));
+        tg.putString(x_start, y_start - 2, GeneralNamesMap.getName(general.getActionType()));
+        tg.setForegroundColor(TextColorMap.getColor("white"));
     }
 }
