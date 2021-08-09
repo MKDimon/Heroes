a2e6f47f0705a29fd0ea540c8a5e2fbb068799da
Gleb <glebtimoschenko@gmail.com>
2021-08-09 14:50:33 +0600

    + MultithreadedExpectiMaxBot fix

--- src/main/java/heroes/player/botgleb/MultithreadedExpectiMaxBot.java
+++ src/main/java/heroes/player/botgleb/MultithreadedExpectiMaxBot.java
@@ -1,16 +1,12 @@
 package heroes.player.botgleb;
 
 import heroes.auxiliaryclasses.gamelogicexception.GameLogicException;
-import heroes.auxiliaryclasses.gamelogicexception.GameLogicExceptionType;
-import heroes.gamelogic.Army;
 import heroes.gamelogic.Board;
 import heroes.gamelogic.Fields;
 import heroes.gamelogic.GameStatus;
 import heroes.gui.TerminalWrapper;
 import heroes.gui.Visualisable;
 import heroes.player.Answer;
-import heroes.player.BaseBot;
-import heroes.player.TestBot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,40 +18,27 @@ import java.util.concurrent.RecursiveTask;
 import java.util.function.ToDoubleFunction;
 
 /**
- * Бот по стратегии "минимакс" с использованием ForkJoinPull.
+ * Бот по стратегии "ExpectiMax" с использованием ForkJoinPull.
  **/
 
-public class MultithreadedExpectiMaxBot extends BaseBot implements Visualisable {
-
-    private static final int maxRecLevel = 3;
-    private static final UtilityFunction utilityFunction = UtilityFunctions.HPUtilityFunction;
-
-    private static final Logger logger = LoggerFactory.getLogger(SimpleMinMaxBot.class);
-
+public class MultithreadedExpectiMaxBot extends ExpectiMaxBot implements Visualisable {
+    private static final Logger logger = LoggerFactory.getLogger(MultithreadedExpectiMaxBot.class);
     protected final TerminalWrapper tw = null;
 
     /**
      * Фабрика ботов.
      **/
 
-    public static class MultithreadedExpectiMaxBotFactory extends BaseBotFactory {
+    public static class MultithreadedExpectiMaxBotFactory extends AIBotFactory {
         @Override
         public MultithreadedExpectiMaxBot createBot(final Fields fields) throws GameLogicException {
             return new MultithreadedExpectiMaxBot(fields);
         }
-    }
 
-    /**
-     * Вспомогательный класс. Хранит ответ и оценку состояния доски, к которому приведет этот ответ.
-     **/
-
-    private static final class AnswerAndWin {
-        final Answer answer;
-        final double win;
-
-        private AnswerAndWin(final Answer answer, final double win) {
-            this.answer = answer;
-            this.win = win;
+        @Override
+        public MultithreadedExpectiMaxBot createAIBot(final Fields fields, final UtilityFunction utilityFunction,
+                                                      final int maxRecLevel) throws GameLogicException {
+            return new MultithreadedExpectiMaxBot(fields, utilityFunction, maxRecLevel);
         }
     }
 
@@ -63,13 +46,13 @@ public class MultithreadedExpectiMaxBot extends BaseBot implements Visualisable 
      * Класс "распараллеливвтель" задач. Метод compute() строит дерево игры.
      **/
 
-    private final class WinCounter extends RecursiveTask<AnswerAndWin> {
+    private final class ExpectiMaxWinCounter extends RecursiveTask<AnswerAndWin> {
 
         private final Board implBoard;
         private final int recLevel;
         private final Answer rootAnswer;
 
-        private WinCounter(final Board implBoard, final int recLevel, final Answer rootAnswer) {
+        private ExpectiMaxWinCounter(final Board implBoard, final int recLevel, final Answer rootAnswer) {
             this.implBoard = implBoard;
 
             this.recLevel = recLevel;
@@ -85,22 +68,22 @@ public class MultithreadedExpectiMaxBot extends BaseBot implements Visualisable 
                 if (implBoard.getStatus() != GameStatus.GAME_PROCESS) {
                     return new AnswerAndWin(rootAnswer, getTerminalStateValue(implBoard));
                 }
-                if (recLevel >= maxRecLevel) {
+                if (recLevel >= getMaxRecLevel()) {
                     // функция полезности вычисляется для агента.
                     // Показывает, насколько поелзно будет ему это действие
-                    return new AnswerAndWin(rootAnswer, utilityFunction.compute(implBoard, getField()));
+                    return new AnswerAndWin(rootAnswer, getUtilityFunction().compute(implBoard, getField()));
                 }
                 // Если состояние не терминальное, и не достигнут максимлаьынй уровень рекурсии,
                 // то начинаем строить дерево из текущего состояния.
                 final List<Answer> actions = implBoard.getPossibleMoves();
                 final List<AnswerAndWin> awList = new ArrayList<>();
-                final List<WinCounter> subTasks = new LinkedList<>();
+                final List<ExpectiMaxWinCounter> subTasks = new LinkedList<>();
                 for (final Answer answer : actions) {
-                    final WinCounter task = new WinCounter(implBoard.copy(answer), recLevel + 1, answer);
+                    final ExpectiMaxWinCounter task = new ExpectiMaxWinCounter(implBoard.copy(answer), recLevel + 1, answer);
                     task.fork();
                     subTasks.add(task);
                 }
-                for (final WinCounter subTask : subTasks) {
+                for (final ExpectiMaxWinCounter subTask : subTasks) {
                     final AnswerAndWin aw = subTask.join();
                     awList.add(aw);
                 }
@@ -109,11 +92,12 @@ public class MultithreadedExpectiMaxBot extends BaseBot implements Visualisable 
                 // корневому ответу сопоставляется значение из нижнего состяния.
                 if (rootAnswer != null) {
                     return new AnswerAndWin(rootAnswer, getGreedyDecision(awList, isMax,
-                            aw -> aw.win/ awList.size()).win);
+                            aw -> aw.win() / awList.size()).win());
                 } else {
-                    return getMax(awList);
+                    return getMaxAW(awList);
                 }
             } catch (GameLogicException e) {
+                logger.error("Error computing tree by MultithreadedExpectiMaxBot", e);
                 return null;
             }
         }
@@ -123,19 +107,14 @@ public class MultithreadedExpectiMaxBot extends BaseBot implements Visualisable 
         super(field);
     }
 
-    @Override
-    public void setTerminal(final TerminalWrapper tw) {
-        super.tw = tw;
+    public MultithreadedExpectiMaxBot(final Fields fields, final UtilityFunction utilityFunction,
+                                      final int maxRecLevel) throws GameLogicException {
+        super(fields, utilityFunction, maxRecLevel);
     }
 
     @Override
-    public Army getArmy(final Army firstPlayerArmy) {
-        try {
-            return new TestBot(getField()).getArmy(firstPlayerArmy);
-        } catch (final GameLogicException e) {
-            logger.error("Error creating army by MultiThreadedExpectiMax bot", e);
-            return null;
-        }
+    public void setTerminal(final TerminalWrapper tw) {
+        super.tw = tw;
     }
 
     /**
@@ -145,71 +124,19 @@ public class MultithreadedExpectiMaxBot extends BaseBot implements Visualisable 
     @Override
     public Answer getAnswer(final Board board) throws GameLogicException {
         final long startTime = System.currentTimeMillis();
-        final WinCounter startWinCounter = new WinCounter(board, 0, null);
+        final ExpectiMaxWinCounter startWinCounter = new ExpectiMaxWinCounter(board, 0, null);
         final ForkJoinPool resultForkJoinPool = new ForkJoinPool();
-        final Answer result = resultForkJoinPool.invoke(startWinCounter).answer;
+        final Answer result = resultForkJoinPool.invoke(startWinCounter).answer();
         System.out.println("MultiThreadedExpectiMax time: " + (System.currentTimeMillis() - startTime));
         return result;
-
     }
 
     private AnswerAndWin getGreedyDecision(final List<AnswerAndWin> awList, final boolean isMax,
-                                           final ToDoubleFunction<AnswerAndWin> probabilityFunction){
-        if(isMax){
-            return getMax(awList);
+                                           final ToDoubleFunction<AnswerAndWin> probabilityFunction) {
+        if (isMax) {
+            return getMaxAW(awList);
         } else {
             return new AnswerAndWin(null, getChance(awList, probabilityFunction));
-        }
-    }
-
-    /**
-     * Метод находит в списке awList элемент с максимальным полем win.
-     **/
-
-    private AnswerAndWin getMax(final List<AnswerAndWin> awList) {
-        AnswerAndWin bestAW = awList.get(0);
-        double bestWin = bestAW.win;
-        for (int i = 1; i < awList.size(); i++) {
-            final AnswerAndWin currentAW = awList.get(i);
-            final double currentWin = currentAW.win;
-            if (currentWin > bestWin) {
-                bestAW = currentAW;
-                bestWin = currentWin;
-            }
-        }
-        return bestAW;
-    }
-
-    /**
-     * Метод оценивает вероятностные узлы с помощью функции probabilityFunction.
-     **/
-
-    private double getChance(final List<AnswerAndWin> awList,
-                             final ToDoubleFunction<AnswerAndWin> probabilityFunction) {
-        double win = 0;
-        for (final AnswerAndWin aw : awList) {
-            win += probabilityFunction.applyAsDouble(aw);
-        }
-        return win;
-    }
-
-    /**
-     * Метод вычисляет тип терминального состояния и выдает в соответствии с ним значение функции полезности
-     * (+- условная бесконечность, либо 0, если ничья).
-     **/
-
-    private double getTerminalStateValue(final Board board) throws GameLogicException {
-        if (board.getStatus() == GameStatus.GAME_PROCESS) {
-            throw new GameLogicException(GameLogicExceptionType.INCORRECT_PARAMS);
-        }
-        if (board.getStatus() == GameStatus.NO_WINNERS) {
-            return -100000d;
-        }
-        if (board.getStatus() == GameStatus.PLAYER_ONE_WINS && getField() == Fields.PLAYER_ONE ||
-                board.getStatus() == GameStatus.PLAYER_TWO_WINS && getField() == Fields.PLAYER_TWO) {
-            return UtilityFunctions.MAX_VALUE;
-        } else {
-            return UtilityFunctions.MIN_VALUE;
         }
     }
 
