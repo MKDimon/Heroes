babb9a5df43fb4b5b0e223a0b5c253f4fbda643a
Gleb <glebtimoschenko@gmail.com>
2021-08-09 15:08:41 +0600

    + MonteCarloUtilityFunction

--- src/main/java/heroes/player/botgleb/UtilityFunctions.java
+++ src/main/java/heroes/player/botgleb/UtilityFunctions.java
@@ -1,12 +1,21 @@
 package heroes.player.botgleb;
 
 import heroes.auxiliaryclasses.ActionTypes;
+import heroes.auxiliaryclasses.boardexception.BoardException;
+import heroes.auxiliaryclasses.gamelogicexception.GameLogicException;
+import heroes.auxiliaryclasses.unitexception.UnitException;
 import heroes.gamelogic.Board;
 import heroes.gamelogic.Fields;
+import heroes.gamelogic.GameLogic;
+import heroes.gamelogic.GameStatus;
+import heroes.player.Answer;
+import heroes.player.BaseBot;
+import heroes.player.RandomBot;
 import heroes.units.General;
 import heroes.units.Unit;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class UtilityFunctions {
 
@@ -36,14 +45,14 @@ public class UtilityFunctions {
         return HPOnePlayerUtilityFunction(board, player) - HPOnePlayerUtilityFunction(board, defField);
     };
 
-    private static final double HPOnePlayerUtilityFunction(final Board board, final Fields player) {
+    private static double HPOnePlayerUtilityFunction(final Board board, final Fields player) {
         final Unit[][] playerArmy = board.getArmy(player);
         final General playerGen = board.getGeneral(player);
         double result = 0;
         for (final Unit[] units : playerArmy) {
             for (final Unit unit : units) {
                 if (unit.isAlive()) {
-                    result += unit.getCurrentHP() + ( (double) unit.getPower() * (double) unit.getAccuracy()/100)
+                    result += unit.getCurrentHP() + ((double) unit.getPower() * (double) unit.getAccuracy() / 100)
                             - unit.getDefenseArmor();
                     if (unit.getActionType() == ActionTypes.HEALING) {
                         result += 500;
@@ -61,4 +70,34 @@ public class UtilityFunctions {
         }
         return result;
     }
+
+    public static final UtilityFunction MonteCarloUtilityFunction = (board, player) -> {
+        try {
+            final Fields enemy = player == Fields.PLAYER_TWO ? Fields.PLAYER_ONE : Fields.PLAYER_TWO;
+            final BaseBot botPlayer = new RandomBot(player);
+            final BaseBot botEnemy = new RandomBot(enemy);
+            final Map<Fields, BaseBot> getPlayer = Map.of(
+                    player, botPlayer,
+                    enemy, botEnemy
+            );
+            final int iterations = 1000;
+            int result = 0;
+            for (int i = 0; i < iterations; i++) {
+                final GameLogic simulationGL = new GameLogic(board);
+                while (simulationGL.isGameBegun()) {
+                    final Answer answer = getPlayer.get(simulationGL.getBoard().getCurrentPlayer()).
+                            getAnswer(simulationGL.getBoard());
+                    simulationGL.action(answer.getAttacker(), answer.getDefender(), answer.getActionType());
+                }
+                if (simulationGL.getBoard().getStatus() == GameStatus.PLAYER_ONE_WINS && player == Fields.PLAYER_ONE
+                        || simulationGL.getBoard().getStatus() == GameStatus.PLAYER_TWO_WINS
+                        && player == Fields.PLAYER_TWO) {
+                    result ++;
+                }
+            }
+            return (double) result / iterations;
+        } catch (final GameLogicException | UnitException | BoardException e) {
+            return 0;
+        }
+    };
 }
