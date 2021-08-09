b7025d629bf113edbd9898f0a12bc55f1c58a700
Simakov Nikita <simakovnikita4@gmail.com>
2021-08-09 12:19:40 +0600

    [*] Fixed issues of pull request #38

--- src/main/java/heroes/player/controlsystem/Selector.java
+++ src/main/java/heroes/player/controlsystem/Selector.java
@@ -39,8 +39,8 @@ public class Selector {
     public void updateSelection(final KeyType kt) {
         Pair<Integer, Integer> fromMap = keyTypePairMap.getOrDefault(kt, new Pair<>(0, 0));
 
-        int x = (fromMap.getX() + currentSelection.getX() + rows) % rows; // java % - ост. от деления, нужно добавить
-        int y = (fromMap.getY() + currentSelection.getY() + cols) % cols; // чтобы не уйти в отрицательные числа
+        final int x = (fromMap.getX() + currentSelection.getX() + rows) % rows;
+        final int y = (fromMap.getY() + currentSelection.getY() + cols) % cols;
 
         currentSelection = new Pair<>(x, y);
     }
