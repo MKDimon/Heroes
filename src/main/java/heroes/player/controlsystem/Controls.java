bb0f0d2b08de68639cc4622133ade2df873f6ea6
Gleb <glebtimoschenko@gmail.com>
2021-08-12 10:50:24 +0600

    + Controls fix

--- src/main/java/heroes/player/controlsystem/Controls.java
+++ src/main/java/heroes/player/controlsystem/Controls.java
@@ -2,24 +2,24 @@ package heroes.player.controlsystem;
 
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
-import heroes.gui.heroeslanterna.LanternaWrapper;
+import heroes.gui.IGUI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class Controls {
-    private final LanternaWrapper tw;
+    private final IGUI gui;
 
     private final Logger logger = LoggerFactory.getLogger(Controls.class);
-    public Controls(final LanternaWrapper tw) {
-        this.tw = tw;
+    public Controls(final IGUI gui) {
+        this.gui = gui;
     }
 
     public KeyType update() {
         KeyStroke ks = null;
         try {
-            ks = tw.getScreen().pollInput();
+            ks = gui.getScreen().pollInput();
         } catch (IOException e) {
             logger.error("Error in getting pressed button procedure in Controls.", e);
         }
