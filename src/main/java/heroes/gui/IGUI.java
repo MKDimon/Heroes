f613b9d4bf47a59aa16e25ed2caf4d0262cc85fa
Gleb <glebtimoschenko@gmail.com>
2021-08-12 11:06:20 +0600

    + IGUI fix

--- src/main/java/heroes/gui/IGUI.java
+++ src/main/java/heroes/gui/IGUI.java
@@ -1,9 +1,5 @@
 package heroes.gui;
 
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.terminal.Terminal;
 import heroes.gamelogic.Board;
 import heroes.gamelogic.Fields;
 import heroes.player.Answer;
@@ -11,13 +7,10 @@ import heroes.player.Answer;
 public interface IGUI {
     void refresh();
     void start();
-    TextGraphics newTG();
     void printPlayer(final Fields field);
     int updateMenu();
     void update(final Answer answer, final Board board);
     void stop();
-    Screen getScreen();
-    Terminal getTerminal();
-    TerminalSize getTerminalSize();
-
+    void clear();
+    void pollInput();
 }
