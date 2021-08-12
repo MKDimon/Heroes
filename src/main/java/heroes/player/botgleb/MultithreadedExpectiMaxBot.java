74ebfe34422afc282fa1cecf118296418e6a93b1
Gleb <glebtimoschenko@gmail.com>
2021-08-12 17:16:49 +0600

    + MultithreadedExpectiMaxBot LanternaWrapper delete

--- src/main/java/heroes/player/botgleb/MultithreadedExpectiMaxBot.java
+++ src/main/java/heroes/player/botgleb/MultithreadedExpectiMaxBot.java
@@ -5,7 +5,6 @@ import heroes.gamelogic.Board;
 import heroes.gamelogic.Fields;
 import heroes.gamelogic.GameStatus;
 import heroes.gui.Visualisable;
-import heroes.gui.heroeslanterna.LanternaWrapper;
 import heroes.player.Answer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +22,6 @@ import java.util.function.ToDoubleFunction;
 
 public class MultithreadedExpectiMaxBot extends ExpectiMaxBot implements Visualisable {
     private static final Logger logger = LoggerFactory.getLogger(MultithreadedExpectiMaxBot.class);
-    protected final LanternaWrapper tw = null;
 
     /**
      * Фабрика ботов.
@@ -110,11 +108,6 @@ public class MultithreadedExpectiMaxBot extends ExpectiMaxBot implements Visuali
     public MultithreadedExpectiMaxBot(final Fields fields, final UtilityFunction utilityFunction,
                                       final int maxRecLevel) throws GameLogicException {
         super(fields, utilityFunction, maxRecLevel);
-    }
-
-    @Override
-    public void setTerminal(final LanternaWrapper tw) {
-        super.tw = tw;
     }
 
     /**
